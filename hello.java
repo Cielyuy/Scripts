c62b0b44be4300f7620e1c2eb13d79ec5c47b27a
pangyao <482318285@qq.com>
2021-07-26 17:05:57 +0800

    123

---
+++ hello.java
@@ -0,0 +1,12 @@
+// class t1 {
+//     int a = 1;
+//     void getNum(){
+//         System.out.println(a);
+//     }
+// }
+
+public class hello {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
